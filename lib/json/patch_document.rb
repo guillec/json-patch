a85d84128c915d9b49d0506e51e74283a7743a6a
Guille Carlos <guille@bitpop.in>
2013-05-18 17:26:39 -0400

    First pass done

--- lib/json/patch_document.rb
+++ lib/json/patch_document.rb
@@ -16,129 +16,111 @@ module JSON
   end
 
   private
-  def self.add(target_document, operation_document)
-    raise JSON::PatchError if operation_document["value"] == nil
-    json_pointer    = operation_document["path"]
-    path_array      = split_path(json_pointer)
-    reference_token = path_array.pop
-    target_item     = build_target_array(path_array, target_document)
-    add_operation(target_document, path_array, target_item, reference_token, operation_document["value"])
-    JSON.dump(target_document)
+  def self.add(target_doc, operation_doc)
+    raise JSON::PatchError if operation_doc["value"] == nil
+    path  = operation_doc["path"]
+    value = operation_doc["value"]
+
+    add_operation(target_doc, path, value)
+    JSON.dump(target_doc)
   end
 
-  def self.remove(target_document, operation_document)
-    raise JSON::PatchError if operation_document["path"] == nil
-    json_pointer    = operation_document["path"]
-    path_array      = split_path(json_pointer)
-    reference_token = path_array.pop
-    target_item     = build_target_array(path_array, target_document)
-    remove_operation(target_document, path_array, target_item, reference_token)
-    JSON.dump(target_document)
+  def self.remove(target_doc, operation_doc)
+    raise JSON::PatchError if operation_doc["path"] == nil
+    path = operation_doc["path"]
+
+    remove_operation(target_doc, path)
+    JSON.dump(target_doc)
   end
 
-  def self.replace(target_document, operation_document)
-    raise JSON::PatchError if operation_document["path"] == nil
-    raise JSON::PatchError if operation_document["value"] == nil
-    #json_pointer    = operation_document["path"]
-    #path_array      = split_path(json_pointer)
-    #reference_token = path_array.pop
-    #dest            = build_target_array(path_array, target_document)
-    #remove_operation(target_document, path_array, dest, reference_token)
-    #add_operation(target_document, path_array, dest, reference_token, operation_document["value"])
-    remove(target_document, operation_document)
-    add(target_document, operation_document)
-    JSON.dump(target_document)
+  def self.replace(target_doc, operation_doc)
+    raise JSON::PatchError if operation_doc["path"] == nil
+    raise JSON::PatchError if operation_doc["value"] == nil
+
+    remove(target_doc, operation_doc)
+    add(target_doc, operation_doc)
+    JSON.dump(target_doc)
   end
 
-  def self.move(target_document, operation_document)
-    raise JSON::PatchError if operation_document["from"] == nil
-    from_json_pointer    = operation_document["from"]
-    from_path_array      = split_path(from_json_pointer)
-    from_reference_token = from_path_array.pop
-    from                 = build_target_array(from_path_array, target_document)
-    moving_value         = remove_operation(target_document, from_path_array, from, from_reference_token)
+  def self.move(target_doc, operation_doc)
+    raise JSON::PatchError if operation_doc["from"] == nil
+    src   = operation_doc["from"]
+    dest  = operation_doc["path"]
+    value = remove_operation(target_doc, src)
 
-    to_json_pointer      = operation_document["path"]
-    to_path_array        = split_path(to_json_pointer)
-    to_reference_token   = to_path_array.pop
-    to                   = build_target_array(to_path_array, target_document)
-    add_operation(target_document, to_path_array, to, to_reference_token, moving_value)
-
-    JSON.dump(target_document)
+    add_operation(target_doc, dest, value)
+    JSON.dump(target_doc)
   end
 
-  def self.copy(target_document, operation_document)
-    raise JSON::PatchError if operation_document["from"] == nil
-    temp_doc             = target_document.clone
-    from_json_pointer    = operation_document["from"]
-    from_path_array      = split_path(from_json_pointer)
-    from_reference_token = from_path_array.pop
-    from                 = build_target_array(from_path_array, temp_doc)
-    moving_value         = get_value_at(temp_doc, from_path_array, from, from_reference_token)
+  def self.copy(target_doc, operation_doc)
+    raise JSON::PatchError if operation_doc["from"] == nil
+    src   = operation_doc["from"]
+    dest  = operation_doc["path"]
+    value = find_value(target_doc, operation_doc, src)
 
-    to_json_pointer      = operation_document["path"]
-    to_path_array        = split_path(to_json_pointer)
-    to_reference_token   = to_path_array.pop
-    to                   = build_target_array(to_path_array, target_document)
-    add_operation(target_document, to_path_array, to, to_reference_token, moving_value)
-    JSON.dump(target_document)
+    add_operation(target_doc, dest, value)
+    JSON.dump(target_doc)
   end
 
-  def self.test(target_document, operation_document)
-    raise JSON::PatchError if operation_document["value"] == nil
-    json_pointer    = operation_document["path"]
-    path_array      = split_path(json_pointer)
-    reference_token = path_array.pop
-    dest            = build_target_array(path_array, target_document)
-    value           = get_value_at(target_document, path_array, dest, reference_token)
-    value == operation_document["value"]
+  def self.test(target_doc, operation_doc)
+    raise JSON::PatchError if operation_doc["value"] == nil
+    path       = operation_doc["path"]
+    value      = find_value(target_doc, operation_doc, path)
+    test_value = operation_doc["value"]
+
+    value == test_value
   end
 
+  def self.add_operation(target_doc, path, value)
+    path_array  = split_path(path)
+    ref_token   = path_array.pop
+    target_item = build_target_array(path_array, target_doc)
 
-  def self.build_target_array(path_array, object)
-    path_array.inject(object) do |obj, part|
-      obj[(Array === obj ? part.to_i : part)]
-    end
-  end
-
-  def self.remove_operation(obj, path, dest, reference_token)
-    if Array === dest
-      dest.delete_at reference_token.to_i
-    else
-      obj.delete reference_token
-    end
-  end
-
-  def self.remove_operation(obj, path, dest, reference_token)
-    if Array === dest
-      dest.delete_at reference_token.to_i
-    else
-      dest.delete reference_token
-    end
-  end
-
-  def self.get_value_at(obj, path, dest, reference_token)
-    if Array === dest
-      dest.at reference_token.to_i
-    else
-      dest[reference_token]
-    end
-  end
-
-  def self.add_operation(target_doc, path, target_item, reference_token, value)
     if Array === target_item
-      new_array = target_item.insert reference_token.to_i, value
-      return self.add_to_target_document(target_doc, path, target_item, new_array)
+      new_array = target_item.insert ref_token.to_i, value
+      add_to_target_document(target_doc, path_array, target_item, new_array)
     else
-      target_item[reference_token] = value
+      target_item[ref_token] = value
     end
   end
 
-  def self.add_to_target_document(doc, path, target_item, new_array)
+  def self.remove_operation(target_doc, path)
+    path_array  = split_path(path)
+    ref_token   = path_array.pop
+    target_item = build_target_array(path_array, target_doc)
+
+    if Array === target_item
+      target_item.delete_at ref_token.to_i
+    else
+      target_item.delete ref_token
+    end
+  end
+
+  def self.find_value(target_doc, operation_doc, path)
+    path_array  = split_path(path)
+    ref_token   = path_array.pop
+    target_item = build_target_array(path_array, target_doc)
+
+    if Array === target_item
+      target_item.at ref_token.to_i
+    else
+      target_item[ref_token]
+    end
+  end
+
+  def self.build_target_array(path_array, target_doc)
+    path_array.inject(target_doc) do |doc, item|
+      key = (Array === doc ? item.to_i : item)
+      doc[key]
+    end
+  end
+
+  def self.add_to_target_document(doc, path, target_item, array)
     path.inject(doc) do |obj, part|
-      doc[(Array === doc ? part.to_i : part)]
+      key = (Array === doc ? part.to_i : part)
+      doc[key]
       if part == path.last
-        doc[part] = new_array
+        doc[part] = array
       end
     end
   end
